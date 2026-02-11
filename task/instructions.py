422ec9555bef5cf0fa4095ff580433343e23f7e5
Antoniya <antoniya.boyanova.15@gmail.com>
2026-02-11 09:27:50 +0100

    created the design matrix and a more or less final version of the task

---
+++ task/instructions.py
@@ -0,0 +1,72 @@
+# instructions.py
+
+INTRO_TEXT = """
+👋 WELCOME AND THANK YOU FOR TAKING PART IN THIS EXPERIMENT👋
+
+In this task, you will see a series of very brief images
+presented on the left and right side of the screen 👀
+
+Each trial begins with a word cue shown at the center
+of the screen 
+
+The cue indicates the type of image that is MORE LIKELY
+to appear on that trial — but it is NOT always correct 🤔
+
+The cues you will see are:
+
+▶  Sea animal
+▶  Water vessel
+▶  Neutral
+
+After the cue, two images will appear very briefly:
+one on the left and one on the right 
+They will be immediately followed by visual masks ⏱
+
+⚠ IMPORTANT ⚠
+Please keep your eyes on the + fixation cross
+at the center of the screen at all times 🎯
+
+Press SPACE to continue ⌨
+"""
+
+
+TASK_TEXT = """
+On each trial, you will complete two responses.
+
+1. LOCATION response:
+Where did an image appear?
+Press LEFT if the target appeared on the left.
+Press RIGHT if the target appeared on the right.
+
+2. IDENTITY response:
+Which exact image appear? 
+Use LEFT and RIGHT to move the selection frame.
+Press UP to confirm your choice.
+
+Please respond based on your first impression.
+
+⚠ IMPORTANT ⚠
+Sometimes it will be really hard but still try to 
+give your best guess!🎯
+
+Press SPACE to continue ⌨
+"""
+
+BREAK_TEXT = """
+Please take a short break (2–3 minutes).
+
+Press SPACE to continue ⌨
+"""
+
+PRACTICE_TEXT = """
+Now you will be shown a short practice of the task, 
+so that you can get used to it!
+
+Press SPACE to continue ⌨
+"""
+
+START_TEXT = """
+If you have any questions, please ask the experimenter now.
+
+Press SPACE to START the experiment ⌨ 
+"""
