1a558cd691daaf048b70b8d8eb054b0eb3e83ea8
Antoniya <antoniya.boyanova.15@gmail.com>
2026-03-23 19:24:41 +0100

    initial commit to the eye-tracking implementation

---
+++ 01_Eyelink_Task/instructions.py
@@ -0,0 +1,82 @@
+# instructions.py
+
+INTRO_TEXT = """
+👋 WELCOME AND THANK YOU FOR TAKING PART IN THIS EXPERIMENT👋
+
+In this task, you will see a series of very brief images
+presented on the left and right side of the screen 👀
+
+Each trial begins with a word cue 
+shown at the center of the screen. 
+
+The cue indicates the type of image that is MORE LIKELY
+to appear on that trial — but it is NOT always correct 🤔
+
+The cues you will see are:
+
+▶  Sea animal
+▶  Water vessel
+▶  Neutral
+
+After the cue, two images will appear simultaneously very briefly:
+one on the LEFT and one on the RIGHT                 
+They will be immediately followed by visual masks ⏱
+
+⚠ IMPORTANT ⚠
+Please keep your eyes on the + fixation cross
+at the center of the screen at all times 🎯
+
+Press SPACE to continue ⌨
+"""
+
+
+TASK_TEXT = """
+On each trial, you will complete two responses.
+---------------------------------------------------
+=== 1. LOCATION response ===
+WHERE did an image appear?
+
+Once the left and right images disappear, the fixation cross will turn into:
+<< >>
+At this point you need to:
+← →
+Press LEFT if you saw an image on the left side.
+Press RIGHT if you saw an image on the right side.
+
+---------------------------------------------------
+=== 2. IDENTITY response === 
+WHAT exact image appeared? 
+
+Once you responded left or right, you will be shown 4 images which 
+will be shown in a cardinal way.
+At this point you need to:
+← → ↑ ↓
+Press an arrow key (LEFT, RIGHT, UP, DOWN) to select the image you saw.
+ 
+---------------------------------------------------
+
+⚠ IMPORTANT ⚠
+Please respond fast and based on your first impression!
+Sometimes it will be really hard but try to guess!🎯
+
+Press SPACE to continue ⌨
+"""
+
+BREAK_TEXT = """
+Please take a short break (1–2 minutes).
+
+Press SPACE to continue ⌨
+"""
+
+PRACTICE_TEXT = """
+Now you will be shown a short practice of the task, 
+so that you can get used to it!
+
+Press SPACE to continue ⌨
+"""
+
+START_TEXT = """
+If you have any questions, please ask the experimenter now.
+
+Press SPACE to START the experiment ⌨ 
+"""
